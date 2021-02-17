3d8b6e13af4cdcb359855511ddbc7b6947fb8363
DarraghMakesGames <DarraghMakesGames@users.noreply.github.com>
2021-02-17 08:19:04 +0000

    Added "Temple" scene

---
+++ MegaGolf/Assets/Scripts/BenchFallTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BenchFallTrigger : MonoBehaviour
+{
+
+    [SerializeField] private UnityEvent collidedBench;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collidedBench.Invoke();
+    }
+
+}
